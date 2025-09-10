4b31b3c59596279feda1f1fe7525a90549a98890
spraiedutech <spraiedutech@gmail.com>
2025-09-10 13:09:42 +0000

    background looks static, can you make it more alive and buttons are also

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import MainView from '@/components/app/main-view';
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen w-full flex-col bg-background">
+    <div className="flex min-h-screen w-full flex-col animated-gradient">
       <Header />
       <main className="container mx-auto flex-1 px-4 py-8 md:px-6">
         <MainView />
